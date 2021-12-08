7ec0041aa55066b1f596a2bef13d1f19827661b8
Rob Foulkrod <RobFoulkrod@microsoft.com>
2021-12-08 14:26:48 -0500

    initial commit

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace wolverine.web.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
